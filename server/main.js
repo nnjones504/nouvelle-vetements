835f7b64668826f5ac6c9bd530bdcbc353f35657
Natalie Jones <nnjones99@gmail.com>
2022-01-12 12:07:49 -0600

    updated server ports

--- server/main.js
+++ server/main.js
@@ -1,28 +1,24 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config() 
 
 const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors());
-app.use(express.static("client"));
 
 //import controller file
 const controller = require("./controller");
 
-app.get('/', (req,res) => {
-    res.sendFile("index.html")
-})
-
 app.get(`/api/quote`, controller.getQuote);
 app.get(`/api/`, controller.getProducts);
 app.post(`/api/bag`, controller.addToBag);
-app.get(`/api/bag`,controller.getBag);
-app.post(`/api/reservation`, controller.submitForm)
-app.delete(`/api/bag/:id`, controller.deleteProduct)
+app.get(`/api/bag`, controller.getBag);
+app.post(`/api/reservation`, controller.submitForm);
+app.delete(`/api/bag/:id`, controller.deleteProduct);
 
-
-const port = process.env.PORT || process.env.SERVER_PORT;
-app.listen(port, () => console.log(`Server running on PORT:${port}`));
+app.get("/", function (res, req) {
+  res.send("working");
+});
+const port = 4004 || process.env.PORT;
+app.listen(port, () => console.log("Server running on PORT:4004"));
